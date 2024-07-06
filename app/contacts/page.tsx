f60a877e140cf5687c78cc153b7c967d07788a35
Jay Kothari <jaykothari.d@gmail.com>
2024-07-07 04:31:34 +0530

    show contacts page with data (#2)
    * show contacts page with data
    
    * fix build attempt -1

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-    return <h1>This is the Personal Contacts page</h1>
+import ContactList from "@/components/ContactList";
+import React from "react";
+
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function getData() {
+    return await prisma.contacts.findMany()
+}
+
+export default async function Page() {
+    const contactList = await getData()
+    return (
+        <div>
+           <ContactList contacts={contactList}/>
+        </div>
+    )
 }
--- app/meetings/page.tsx
+++ app/meetings/page.tsx
@@ -2,7 +2,6 @@ export default function Page() {
     return (
         <div>
             This is the meetings page
-
         </div>
     );
 }
